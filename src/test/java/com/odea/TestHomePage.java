1cccff46db8bafa89b18cecf2007dd6f149ac50e
pbergonzi <pbergonzi@odeasrl.com.ar>
2012-10-10 13:02:09 -0300

    integrado unit test de wicket + unit test de spring

--- src/test/java/com/odea/TestHomePage.java
+++ src/test/java/com/odea/TestHomePage.java
@@ -1,20 +1,32 @@
 package com.odea;
 
+import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Simple test using the WicketTester
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:applicationContext.xml"})
 public class TestHomePage
 {
 	private WicketTester tester;
+    @Autowired
+    private ApplicationContext applicationContext;
 
 	@Before
 	public void setUp()
 	{
-		tester = new WicketTester(new WicketApplication());
+        this.tester = new WicketTester();
+        SpringComponentInjector springComponentInjector = new SpringComponentInjector(this.tester.getApplication(), this.applicationContext);
+        this.tester.getApplication().getComponentInstantiationListeners().add(springComponentInjector);
 	}
 
 	@Test
