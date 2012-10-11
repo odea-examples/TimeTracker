30e9fd4956320b68be29769e9fc598189f2b73c4
pbergonzi <pbergonzi@odeasrl.com.ar>
2012-10-11 11:19:32 -0300

    agregado test de encoding

--- src/test/java/com/odea/LoginTest.java
+++ src/test/java/com/odea/LoginTest.java
@@ -1,18 +1,28 @@
 package com.odea;
 
-import static org.junit.Assert.*;
-
+import com.odea.services.EncodingService;
+import com.odea.services.LoginService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.odea.services.LoginService;
+import static org.junit.Assert.*;
 
-public class LoginTest extends AbstractTestCase{
-	@Autowired
-	LoginService loginService;
-	@Test
-	public void loginTest(){
-		assertTrue(loginService.login("Pablo", "bjkbvhjn"));
-	}
+public class LoginTest extends AbstractTestCase {
+    @Autowired
+    LoginService loginService;
+    @Autowired
+    EncodingService encodingService;
 
+    @Test
+    public void loginTest() {
+        assertTrue(loginService.login("Pablo", "bjkbvhjn"));
+    }
+
+    @Test
+    public void hashTest() {
+        String plain1 = "prueba";
+        String plain2 = "prueba2";
+        assertEquals(encodingService.encode(plain1), encodingService.encode(plain1));
+        assertFalse(encodingService.encode(plain1).equals(encodingService.encode(plain2)));
+    }
 }
