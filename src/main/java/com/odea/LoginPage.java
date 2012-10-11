c4d1f33d2e2c71e72e310240f371a6bcdff4514f
gmazzei <gmazzei@odeasrl.com.ar>
2012-10-11 14:31:31 -0300

    Data Base

--- src/main/java/com/odea/LoginPage.java
+++ src/main/java/com/odea/LoginPage.java
@@ -1,10 +1,13 @@
 package com.odea;
 
-import com.odea.services.LoginService;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import com.odea.services.LoginService;
+
+
 
 /**
  * User: pbergonzi
