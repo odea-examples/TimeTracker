29d1fbd40c3dc58745068255e5311430f12a3d64
pbergonzi <pbergonzi@odeasrl.com.ar>
2012-10-16 11:40:39 -0300

    Test para verificar los dias de la semana actual

---
+++ src/test/java/com/odea/FechasTest.java
@@ -0,0 +1,20 @@
+package com.odea;
+
+import org.joda.time.DateTimeConstants;
+import org.joda.time.LocalDate;
+import org.junit.Test;
+
+/**
+ * User: pbergonzi
+ * Date: 16/10/12
+ * Time: 11:36
+ */
+
+public class FechasTest {
+    @Test
+    public void testFechasSemana() {
+        LocalDate now = new LocalDate();
+        System.out.println(now.withDayOfWeek(DateTimeConstants.MONDAY));
+        System.out.println(now.withDayOfWeek(DateTimeConstants.FRIDAY));
+    }
+}
