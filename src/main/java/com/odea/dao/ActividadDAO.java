93c30a65456f9f07c9f7c0a49b9db08f944a857f
PabloGotelli <pgotelli@odeasrl.com.ar>
2012-10-25 13:14:23 -0300

    pequeño cambio

--- src/main/java/com/odea/dao/ActividadDAO.java
+++ src/main/java/com/odea/dao/ActividadDAO.java
@@ -18,7 +18,7 @@ public class ActividadDAO extends AbstractDAO {
 		List<Actividad> actividades = jdbcTemplate.query("SELECT pa.ab_id_a, a.a_name FROM activities a, activity_bind pa WHERE pa.ab_id_a = a.a_id AND pa.ab_id_p =" + proyecto.getIdProyecto(), new RowMapper<Actividad>() {
 			@Override
 			public Actividad mapRow(ResultSet rs, int rowNum) throws SQLException {
-				return new Actividad(rs.getInt(1), rs.getString(2));
+				return new Actividad(rs.getInt(1), rs.getString(3));
 			}
 		});
 		
