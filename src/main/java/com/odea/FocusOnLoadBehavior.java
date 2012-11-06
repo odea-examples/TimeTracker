5f4274ab430366ce1400431be15901aa235151b0
Pablo Bergonzi <pablobergonzi@gmail.com>
2012-11-06 11:50:46 -0300

    Listo el behavior para asignar el focus a cualquier componente

--- src/main/java/com/odea/FocusOnLoadBehavior.java
+++ src/main/java/com/odea/FocusOnLoadBehavior.java
@@ -7,17 +7,18 @@ import org.apache.wicket.behavior.Behavior;
 import org.apache.wicket.markup.head.JavaScriptContentHeaderItem;
 import org.apache.wicket.request.Response;
 
-public class FocusOnLoadBehavior extends Behavior
-{
+public class FocusOnLoadBehavior extends Behavior {
+    @Override
+    public void bind(Component component) {
+        component.setOutputMarkupId(true);
+    }
 
     @Override
     public void afterRender(Component component) {
-        super.afterRender(component);
-        component.setOutputMarkupId(true);
-        String resource = "document.getElementById('" + component.getMarkupId() + "').focus()";
+        String resource = "document.getElementById('" + component.getMarkupId() + "').focus();";
         String uniqueName = Long.toString(Calendar.getInstance().getTimeInMillis());
         Response response = component.getResponse();
-        new JavaScriptContentHeaderItem(resource,uniqueName,null).render(response);
+        new JavaScriptContentHeaderItem(resource, uniqueName, null).render(response);
     }
-    
+
 }  
