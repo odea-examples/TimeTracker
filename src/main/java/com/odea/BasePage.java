8576ed4f82d2b415c9ea2329c8d4537185f608a2
PabloGotelli <pgotelli@odeasrl.com.ar>
2012-12-05 12:59:05 -0300

    Agregado logout

--- src/main/java/com/odea/BasePage.java
+++ src/main/java/com/odea/BasePage.java
@@ -1,14 +1,59 @@
 package com.odea;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
+import com.odea.domain.Entrada;
+
 public class BasePage extends WebPage {
+	
 	public BasePage(){
 		super();
+		this.preparePage();
 	}
 	
 	public BasePage(PageParameters parameters){
 		super(parameters);
+		this.preparePage();
 	}
+	private void preparePage(){
+		
+	    
+	    BaseForm form = new BaseForm("formLogout") {
+			@Override
+			protected void onSubmit(AjaxRequestTarget target, BaseForm form) {
+				SecurityUtils.getSubject().logout();
+				setResponsePage(LoginPage.class);
+			}
+		};  
+		add(form);
+	}
+	public abstract class BaseForm extends Form<Entrada> {
+
+		public BaseForm(String id) {
+			super(id);
+			AjaxButton boton = new AjaxButton("logout") {
+
+				@Override
+				protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+					BaseForm.this.onSubmit(target, (BaseForm) form);
+				}
+				
+			};
+			add(boton);
+			this.setOutputMarkupId(true);
+			
+			
+		}
+
+		protected abstract void onSubmit(AjaxRequestTarget target, BaseForm form);
+		
+		}
 }
+
+
