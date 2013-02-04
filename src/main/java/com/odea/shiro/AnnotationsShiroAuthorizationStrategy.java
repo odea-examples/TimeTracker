2aa45a072cb6caa142c72fdbbb310a2c2468d77f
gmazzei <gmazzei@odeasrl.com.ar>
2013-02-04 10:26:11 -0300

    Ocultamiento de DropDownChoice de usuario en caso de no ser admin

--- src/main/java/com/odea/shiro/AnnotationsShiroAuthorizationStrategy.java
+++ src/main/java/com/odea/shiro/AnnotationsShiroAuthorizationStrategy.java
@@ -1,16 +1,17 @@
 package com.odea.shiro;
 
 import org.apache.shiro.SecurityUtils;
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.Component;
 import org.apache.wicket.RestartResponseAtInterceptPageException;
 import org.apache.wicket.authorization.Action;
 import org.apache.wicket.authorization.IAuthorizationStrategy;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.request.component.IRequestableComponent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.odea.LoginPage;
-import com.odea.NoAutorizadoPage;
 
 public class AnnotationsShiroAuthorizationStrategy implements IAuthorizationStrategy
 {
@@ -26,21 +27,28 @@ public class AnnotationsShiroAuthorizationStrategy implements IAuthorizationStra
 
 	@Override
 	public boolean isActionAuthorized(Component component, Action action) {
-//		System.out.println("Pase por isActionAuthorized() - Page: " + component.getPage().getClass() + " - Component ID: " + component.getId());
-//		System.out.println("Action: " + action);
 		
-		String idComponent = component.getId();
-		String accion = action.toString();
-		boolean notLoggedIn = SecurityUtils.getSubject().getPrincipal() == null;
+		boolean loggedIn = SecurityUtils.getSubject().getPrincipal() != null;
 		
-		if (notLoggedIn && !component.getPage().getClass().toString().equals("class com.odea.LoginPage")) {
+		if (loggedIn) {
+			return this.verificarComponente(component, action);
+		} else if (!component.getPage().getClass().toString().equals("class com.odea.LoginPage")) {
 			throw new RestartResponseAtInterceptPageException(new LoginPage());
 		}
-//		else if (component.getPage().getClass().toString().equals("class com.odea.PruebaPage")) {
-//			throw new RestartResponseAtInterceptPageException(new NoAutorizadoPage());
-//		}
 		
-		return true;	
+		
+		//Si sale del if es porque es la LoginPage, entonces todo se renderiza
+		return true;
+		
+		
+		
+		
+		
+		
+//		if ((idComponent == "selectorUsuario") && (accion=="RENDER")){
+//			component.add(new AttributeModifier("style", new Model("display:none")));
+//		}
+			
 		/*
 		 * Lo de abajo permite esconder el boton de modify link, con un par mas de ifs agregados
 		 * se podria hacer que el link no se vea para usuarios comunes.
@@ -50,6 +58,21 @@ public class AnnotationsShiroAuthorizationStrategy implements IAuthorizationStra
 //			component.add(new AttributeModifier("style", new Model("display:none")));
 //		}
 		
+	}
+	
+	private boolean verificarComponente(Component component, Action action) {
+		
+		String usuario = SecurityUtils.getSubject().getPrincipal().toString();
+		String idComponent = component.getId();
+		String accion = action.toString();
+		
+		if (!usuario.equals("admin")) {
+			if (idComponent == "selectorUsuario") {
+				return false;
+			}
+		}
+		
+		return true;
 	}
 
 	
