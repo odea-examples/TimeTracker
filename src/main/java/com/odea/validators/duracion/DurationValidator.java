52e4d48bd99bf8c0de43e88a5fe47230543a09cf
gmazzei <gmazzei@odeasrl.com.ar>
2013-05-13 13:15:03 -0300

    Validación de duración en Formulario de Entradas. Ahora no puede ser mayor a 24 horas. (Aún falta en la Grid)

--- src/main/java/com/odea/validators/duracion/DurationValidator.java
+++ src/main/java/com/odea/validators/duracion/DurationValidator.java
@@ -14,26 +14,50 @@ public class DurationValidator implements IValidator<String>{
 	private final String WRONG_PATTERN 
 	= "^([0]{1,2}+(,[0]{1,2}|:00|\\b))$";
 	
+	private final String HORA_PATTERN 
+	= "^([0-9]{1,2}:[0-5]{1}[0-9]{1})$";
+	
+	private final String DECIMAL_PATTERN 
+	= "^([0-9]{1,2}(,[0-9]{1,2}|\\b))$";
+	
+	
 	private final Pattern duracionPattern;
 	private final Pattern wrongPattern;
+	private final Pattern horaPattern;
+	private final Pattern decimalPattern;
 
 	public DurationValidator() {
 		duracionPattern = Pattern.compile(DURACION_PATTERN);
 		wrongPattern = Pattern.compile(WRONG_PATTERN);
+		horaPattern = Pattern.compile(HORA_PATTERN);
+		decimalPattern = Pattern.compile(DECIMAL_PATTERN);
 	}
 
 	@Override
 	public void validate(IValidatable<String> validatable) {
 		final String duracion = validatable.getValue();
-		 	
-		if (duracionPattern.matcher(duracion).matches() == false) {	
+		Double duracionParseada; 	
+		
+		if (!duracionPattern.matcher(duracion).matches()) {	
 			error(validatable, "Duración con formato equivocado");
-		} 
-		
-		if (wrongPattern.matcher(duracion).matches() == true) {
+		} else if (wrongPattern.matcher(duracion).matches()) {
 			error(validatable, "Duración no puede ser cero.");
+		} else if (decimalPattern.matcher(duracion).matches())  {
+
+			duracionParseada = Double.parseDouble(duracion.replace(',', '.'));
+			if (duracionParseada >= 24) {
+				error(validatable, "Duración no puede ser mayor a 24 horas");
+			}
+			
+		} else if (horaPattern.matcher(duracion).matches())  {
+			int pos = duracion.indexOf(":");
+			String strHora = duracion.substring(0, pos);
+			duracionParseada = Double.parseDouble(strHora);
+			
+			if (duracionParseada >= 24) {
+				error(validatable, "Duración no puede ser mayor a 24 horas");
+			}
 		}
-		
 		
 	}
 	
