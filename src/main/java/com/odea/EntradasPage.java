955b652c1bc0072cb6108c523c7a9e1b80651ad5
PabloGotelli <pgotelli@odeasrl.com.ar>
2013-05-13 11:48:18 -0300

    Selects bloqueados

--- src/main/java/com/odea/EntradasPage.java
+++ src/main/java/com/odea/EntradasPage.java
@@ -670,15 +670,18 @@ public class EntradasPage extends BasePage {
 		
 		String listaSistemaExterno = daoService.getSistemasExternos().toString();
 		String sistemasExternos = listaSistemaExterno.subSequence(1, listaSistemaExterno.length() - 1).toString();
-		
+		// columna:id,nombre,widht,minwidht,maxwidth,cssclass,field,formater,editor,validator,options
 		Columna columna = new Columna("delCol", "Delete", 60, 60, 60, null, "del", "Slick.Formatters.DeleteButton", null, null, null);
 		Columna columna2 = new Columna("duration", "Duracion", 60, 60, 60, "cell-title", "duration", null, "Slick.Editors.Text", "requiredDurationValidator", null);
-		Columna columna3 = new Columna("actividad", "Actividad", 125, 100, 200, "cell-title", "actividad", null, "Slick.Editors.SelectRelatedEditor", "requiredFieldValidator",	actividades);
-		Columna columna4 = new Columna("proyecto", "Proyecto", 135, 100, 200, "cell-title", "proyecto", null, "Slick.Editors.SelectEditor",	"requiredFieldValidator", proyectos);
+		//"Slick.Editors.SelectRelatedEditor" agregar justo despues del primer null, cambia el tipo de editor de la columna
+		Columna columna3 = new Columna("actividad", "Actividad", 125, 100, 200, "cell-title", "actividad", null, null, "requiredFieldValidator",	actividades);
+		//"Slick.Editors.SelectEditor" agregar justo despues del primer null, cambia el tipo de editor de la columna
+		Columna columna4 = new Columna("proyecto", "Proyecto", 135, 100, 200, "cell-title", "proyecto", null, null,	"requiredFieldValidator", proyectos);
 		Columna columna5 = new Columna("fecha", "Fecha", 60, 60, 60, null, "fecha", null, "Slick.Editors.Date", "requiredFieldValidator", null);
 		Columna columna6 = new Columna("ticket", "Ticket", 50, 50, 50, "cell-title", "ticket", null, "Slick.Editors.Text", "ticketBugzillaValidator", null);
 		Columna columna7 = new Columna("ticketExt", "Ticket Externo", 80, 80, 100, "cell-title", "ticketExt", null, "Slick.Editors.TextTicketExt", "ticketExternoValidator", null);
-		Columna columna8 = new Columna("sistExt", "Sistema Externo", 80, 170, 170, "cell-title", "sistExt", null, "Slick.Editors.SelectEditor", null, sistemasExternos);
+		//"Slick.Editors.SelectEditor" agregar justo despues del primer null, cambia el tipo de editor de la columna
+		Columna columna8 = new Columna("sistExt", "Sistema Externo", 80, 170, 170, "cell-title", "sistExt", null, null, null, sistemasExternos);
 		Columna columna9 = new Columna("descripcion", "Nota", 80, 80, 80, null, "descripcion", null, "Slick.Editors.LongText", "descripcionValidator", null);
 		
 		ArrayList<Columna> columnas = new ArrayList<Columna>();
